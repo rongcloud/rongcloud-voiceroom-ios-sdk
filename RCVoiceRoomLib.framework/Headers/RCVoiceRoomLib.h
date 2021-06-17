b8acc373dcaaedf35501cd467fdb951639246ab7
shaoshuai <shaoshuai@rongcloud.cn>
2021-06-17 16:12:39 +0800

    RC

---
+++ RCVoiceRoomLib.framework/Headers/RCVoiceRoomLib.h
@@ -0,0 +1,21 @@
+//
+//  RCVoiceRoomLib.h
+//  RCVoiceRoomLib
+//
+//  Created by shaoshuai on 2021/6/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RCVoiceRoomLib.
+FOUNDATION_EXPORT double RCVoiceRoomLibVersionNumber;
+
+//! Project version string for RCVoiceRoomLib.
+FOUNDATION_EXPORT const unsigned char RCVoiceRoomLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCVoiceRoomLib/PublicHeader.h>
+
+#import <RCVoiceRoomLib/RCVoiceRoomEngine.h>
+#import <RCVoiceRoomLib/RCVoiceRoomDelegate.h>
+#import <RCVoiceRoomLib/RCVoiceSeatInfo.h>
+#import <RCVoiceRoomLib/RCVoiceRoomInfo.h>
